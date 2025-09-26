07361a98bb141476901a6e7763a29784aa95b1f9
boukroun.younes <boukroun.younes@gmail.com>
2025-09-26 23:51:41 +0200

    update to complated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
 const getTodos = () => {
-    fetch(apiUrl + "?_limit=5")
+    fetch(apiUrl + "?_limit=15")
     .then(response => response.json())
     .then(data => {
         data.forEach((todo) => addTodo(todo));
@@ -11,6 +11,7 @@ const getTodos = () => {
 const addTodo = (todo) => {
     const div = document.createElement('div');
             div.appendChild(document.createTextNode(`${todo.title}`));
+            div.classList.add("todo");
             div.setAttribute('data-id', todo.id);
             if (todo.completed) {
                 div.classList.add("done");
@@ -38,9 +39,30 @@ const createTodo = (e) => {
     });
 }
 
+const toggleCompleted = (e) => {
+
+    if (e.target.classList.contains('todo')) {
+        e.target.classList.toggle('done');
+        updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+    }
+}
+
+const updateTodo = (id, completed) => {
+fetch(`${apiUrl}/${id}`, {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ completed })
+})
+.then(response => response.json())
+.then(data => console.log(data));    
+}
+
 const init = () => {
     document.addEventListener('DOMContentLoaded', getTodos);
     document.querySelector('#todo-form').addEventListener('submit', createTodo);
+    document.querySelector('#todo-list').addEventListener('click', toggleCompleted);
 }
 
 
